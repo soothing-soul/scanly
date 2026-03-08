d1675287515e9c104a8fdbf2ff4b22717ffc5f60
Vijay Kumar <vijay@Vijays-MacBook-Air.local>
2026-03-08 12:44:55 +0530

    Initialized Project

---
+++ src/main/java/com/scanly/ScanlyApplication.java
@@ -0,0 +1,13 @@
+package com.scanly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScanlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scanly/ScanlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scanly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScanlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
